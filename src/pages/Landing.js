7626d45589f9038c5ae1f8e6c7c344da1c551651
callmeharis <theharis99@gmail.com>
2024-05-28 21:27:24 +0500

    JOBSTER-FRONTEND || intial code and components

---
+++ src/pages/Landing.js
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
+import main from "../assets/images/main.svg";
+import Wrapper from "../assets/wrappers/LandingPage";
+
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        {/* info */}
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+            illum assumenda nihil odit fuga laboriosam commodi nobis animi qui.
+            Debitis id dicta harum placeat illo voluptatem a quas repellendus
+            ipsum?
+          </p>
+          <Link to="/register" className="btn btn-hero">Login/Register</Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
+};
+export default Landing;
