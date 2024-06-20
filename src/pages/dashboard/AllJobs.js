5dfcace4072c362fc36d4b54b838c760d805cbd0
callmeharis <theharis99@gmail.com>
2024-06-20 20:51:16 +0500

    JOBSTER-REACT-03 || get all jobs and remove job

--- src/pages/dashboard/AllJobs.js
+++ src/pages/dashboard/AllJobs.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import { SearchContainer, JobsContainer } from "../../components";
 
 const AllJobs = () => {
   return (
-    <h1>
-      All Jobs
-    </h1>
-  )
-}
+    <>
+      <SearchContainer />
+      <JobsContainer />
+    </>
+  );
+};
 
-export default AllJobs
+export default AllJobs;
