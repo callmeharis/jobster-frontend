b22f86a01fce7b5a34d22a068ccaf554c3d590a7
callmeharis <theharis99@gmail.com>
2024-06-25 19:22:52 +0500

    JOBSTER-REACT-05 || complete stats data

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { showStats } from "../../features/allJobs/allJobsSlice";
+import { StatsContainer, ChartsContainer, Loading } from "../../components";
 
 const Stats = () => {
+  const dispatch = useDispatch();
+  const { isLoading, monthlyApplications } = useSelector(
+    (store) => store.allJobs
+  );
+  useEffect(() => {
+    dispatch(showStats());
+  }, [dispatch]);
   return (
-    <h1>
-        Stats
-    </h1>
-  )
-}
+    <>
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartsContainer />}
+    </>
+  );
+};
 
-export default Stats
+export default Stats;
